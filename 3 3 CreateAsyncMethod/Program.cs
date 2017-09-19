dc1ae01a82f6515e3351db17243f02be72c525b0
dz33 <Dante.X.Zhi@newegg.com>
2017-09-19 13:02:28 +0800

    async

---
+++ 3 3 CreateAsyncMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3_3_CreateAsyncMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AsyncMain();
+
+            HttpClientTest.Get();
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        static async void AsyncMain()
+        {
+            string s = await TestAsync();
+            Console.WriteLine(s);
+        }
+        static Task<string> TestAsync()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "Hello";
+            });
+        }
+    }
+}
